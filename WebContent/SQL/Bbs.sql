a7bf74fe90469478a14c19ef88d89b3f40c9b787
HyunTori <so699@naver.com>
2021-07-11 23:17:49 +0900

    게시판 데이터 베이스 구축
    - BbsDTO 생성

---
+++ WebContent/SQL/Bbs.sql
@@ -0,0 +1,9 @@
+create table bbs(
+bbsid number,
+bbstitle varchar2(50),
+userid VARCHAR2(20),
+bbsdate DATE,
+bbscontent varchar2(2048),
+bbsAvailable number,
+primary key(bbsid)
+);
---
+++ WebContent/SQL/Member.sql
@@ -0,0 +1,8 @@
+create table member(
+u_id varchar2(20) not null,
+u_pw varchar2(20) not null,
+u_name varchar2(20) not null,
+u_gender varchar2(10) not null,
+u_eamil varchar2(30) not null,
+primary key(u_id)
+);
